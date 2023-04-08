3b056900df68bd07f4899c945f0fbfe9815956a4
Jakub Łyjak <konithir@gmail.com>
2023-04-09 00:37:05 +0200

    EndGameScene funtional

--- Bearded Asteroid/Assets/_Game/Scripts/PlayerStats.cs
+++ Bearded Asteroid/Assets/_Game/Scripts/PlayerStats.cs
@@ -12,6 +12,9 @@ public class PlayerStats : ScriptableObject
     [SerializeField]
     private int _lives;
 
+    [SerializeField]
+    private int _level;
+
     public string Name
     {
         get { return _name; }
@@ -27,5 +30,11 @@ public class PlayerStats : ScriptableObject
     {
         get { return _lives; }
         set { _lives = value; }
+    }
+
+    public int Level
+    {
+        get { return _level; }
+        set { _level = value; }
     }
 }
---
+++ Bearded Asteroid/Assets/_Game/Scripts/UI/EndGameInfoController.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+public class EndGameInfoController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerStats _player;
+
+    [SerializeField]
+    private TextMeshProUGUI _scoreText;
+
+    [SerializeField]
+    private TextMeshProUGUI _levelText;
+
+    [SerializeField]
+    private TextMeshProUGUI _nameText;
+
+    private void Start()
+    {
+        UpdateScore();
+        UpdateLives();
+        UpdateName();
+    }
+
+    public void UpdateScore()
+    {
+        _scoreText.text = _player.Score.ToString();
+    }
+
+    public void UpdateLives()
+    {
+        _levelText.text = _player.Level.ToString();
+    }
+
+    private void UpdateName()
+    {
+        _nameText.text = _player.Name;
+    }
+}
